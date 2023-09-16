1e3220e5eaa33559fbd3cb9d9843d0848858ecd6
Jocelyn Dong <jocelyn021213@gmail.com>
2023-09-15 21:48:51 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,6 +1,7 @@
 /**
  * Solve the FizzBuzz challenge.
  */
+/*
 class FizzBuzz {
 
     public static void main(String[] args) {
@@ -30,5 +31,38 @@ class FizzBuzz {
 
             }
         }
+    }
+}
+*/
+
+
+class Fizzbuzz {
+
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i < 100) {
+
+            i = doFizzBuzz(i);
+        }
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
+        }
+
+        i++;
+        return i;
     }
 }
